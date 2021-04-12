de3da7517dfa9f29b401d2089e29e4cdd87a1727
lusid7899 <82360007+lusid7899@users.noreply.github.com>
2021-04-12 15:44:57 +0530

    Add files via upload

---
+++ src/AverageMarks.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class AverageMarks {
+
+	public static void main(String[] args) {
+Scanner s = new Scanner(System.in);
+String str = s.next();
+int i=s.nextInt(),j=s.nextInt(),k=s.nextInt(),avg;
+avg=(i+j+k)/3;
+System.out.println(str);
+System.out.println(avg);
+
+	}
+
+}
