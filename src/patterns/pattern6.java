dc56ec580bc9c7fb67a480c48efd9152d54b8bbb
lusid7899 <82360007+lusid7899@users.noreply.github.com>
2021-04-13 15:52:02 +0530

    Add files via upload

---
+++ src/patterns/pattern6.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class pattern6 {
+
+	public static void main(String[] args) {
+Scanner s= new Scanner(System.in);
+int n=s.nextInt();
+int i=1;
+ while(i<=n)
+ {
+	 int j=1;
+	int startnumber = i;
+	 while(j<=i)
+	 {
+		 System.out.print(startnumber);
+		 startnumber++;
+		 System.out.print(" ");
+		 j++;
+	 }
+	 System.out.println();
+	 i++;
+ }
+	}
+
+}
