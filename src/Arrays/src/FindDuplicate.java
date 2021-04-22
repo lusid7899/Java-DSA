eff85fc73baaedc486e08f9cde9d310aec7e48da
lusid7899 <82360007+lusid7899@users.noreply.github.com>
2021-04-22 16:57:01 +0530

    Add files via upload

---
+++ src/Arrays/src/FindDuplicate.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class FindDuplicate {
+	public static int takeinput() {
+		Scanner s = new Scanner(System.in);
+		int size = s.nextInt(),n;
+		int arr[]  = new int[size];
+		for(int i = 0;i<size;i++) {
+			arr[i] = s.nextInt();
+		}
+		n = unique(arr);
+		return n;
+	}
+	public static int unique(int input[]) {
+		int n=0;
+		for(int i=0;i<input.length;i++) {
+			int k =0;
+			for(int j=0;j<input.length;j++) {
+				if(input[i]==input[j]) {
+					k++;
+				}
+				
+			}
+			if(k==2)
+			{
+				n = input[i];
+			}	
+		}
+		
+	return n;
+	}
+
+	public static void main(String[] args) {
+Scanner s = new Scanner(System.in);
+int t = s.nextInt();
+int arr[]= new int[t];
+for(int i = 0;i<t;i++) {
+arr[i] =	takeinput();
+	
+}
+for(int i = 0;i<t;i++)
+{
+	System.out.println(arr[i]);
+}
+
+	}
+
+}
---
+++ src/Arrays/src/PairSum.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class PairSum {
+	public static int[] takinginput() {
+	Scanner s = new Scanner(System.in);
+	int size = s.nextInt();
+	int ar[] = new int[size];
+	for(int i = 0;i<size;i++) {
+		ar[i] = s.nextInt();
+	}
+	return ar;
+	}
+    public static int pair(int input []) {
+    	Scanner s = new Scanner(System.in);
+    	int x = s.nextInt(),k=0;
+    	for(int i=0;i<input.length;i++) {
+    		for(int j=i+1;j<input.length;j++) {
+    			if(input[i]+input[j]==x) {
+    				k++;
+    			}
+    		}
+    	}
+    	return k;
+    	
+    }
+	public static void main(String[] args) {
+Scanner s = new Scanner(System.in);
+int t = s.nextInt();
+int pairs[]  = new int[t];
+
+for(int i=0;i<t;i++) {
+	int arr[]=takinginput();
+	pairs[i] = pair(arr);
+}
+for(int i=0;i<t;i++) {
+System.out.println(pairs[i]);
+}
+
+	}
+
+}
